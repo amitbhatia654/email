a45414b6cf941539188f00de264a19c0e090c521
Amit Bhatia <amitbhatia654@gmail.com>
2025-02-01 18:40:46 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "build"), // Ensure output is set
         filename: "bundle.js",
-        publicPath: "/",
+        publicPath: "auto",
 
     },
     module: {
